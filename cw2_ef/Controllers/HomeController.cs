0de3e03c3768b7a58a0077134678084ceee26786
MrHerbat <kocirel206@gmail.com>
2025-09-15 16:02:38 +0200

    completed cw2_ef
    added confirmation before deleting, deleting and editing

--- cw2_ef/Controllers/HomeController.cs
+++ cw2_ef/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using cw2_ef.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Differencing;
 
 namespace cw2_ef.Controllers
 {
@@ -42,9 +43,50 @@ namespace cw2_ef.Controllers
         [HttpPost]
         public IActionResult AddEditor(Editor editor)
         {
-            //zapisanie do bazy danych
+            if (!ModelState.IsValid)
+            {
+                return View(editor);
+            }
+            _context.Editors.Add(editor);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Editors));
+        }
+        public IActionResult ConfirmDelete(int id)
+        {
+            ViewBag.LinkableId = id;
+            return View();
+        }
+        public IActionResult DeleteEditor(int id)
+        {
+            var editorToDelete = _context.Editors.FirstOrDefault(e => e.Id == id);
+            if (editorToDelete != null)
+            {
+                _context.Editors.Remove(editorToDelete);
+                _context.SaveChanges();
+            }
             return RedirectToAction(nameof(Editors));
         }
 
+        [HttpGet]
+        public IActionResult EditEditor(int id)
+        {
+            var editorToEdit = _context.Editors.FirstOrDefault(e => e.Id == id);
+            if (editorToEdit != null)
+            {
+                return View(editorToEdit);
+            }
+            return RedirectToAction(nameof(Editors));
+        }
+        [HttpPost]
+        public IActionResult EditEditor(Editor editor,int id)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(editor);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Editors));
+            }
+            return View(editor);
+        }
     }
 }
