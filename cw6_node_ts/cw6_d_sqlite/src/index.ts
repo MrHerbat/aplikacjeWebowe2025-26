4433e51f98ae09f8e1b52bf7f063105e94dc8bac
MrHerbat <kocirel206@gmail.com>
2025-12-15 16:06:05 +0100

    bfkjsbasfkbs

--- cw6_node_ts/cw6_d_sqlite/src/index.ts
+++ cw6_node_ts/cw6_d_sqlite/src/index.ts
@@ -1,8 +1,26 @@
-import { add } from './functions.js';
-import sqlite3  from 'sqlite3';
-import path from 'path';
-import { fileURLToPath } from 'url';
+// import { hello } from "./functions.js";
+
+// console.log(hello("World"));
+// // console.log(__dirname);
+// // console.log(__filename);
+
+import {  getAllUsers,addUser } from "./SqliteRepo.js";
+import readline from "readline";
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 
-
-console.log('2 + 3 =', add(2, 3));
+const main = async () => {
+    const users = await getAllUsers();
+    console.log(users);
+    const answer1 = await rl.question("Czy dodać nowego użytkownika? (t/n) ")
+        if(answer1.toLowerCase() === 't') {
+        
+        } else {
+            console.log("Koniec programu.");
+        }
+        rl.close();
+};
+main();
